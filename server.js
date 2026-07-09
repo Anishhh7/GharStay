a6bb94bff25bdeed750916e4c6479108f112701d
Anishh <pokharelanish099@gmail.com>
2026-07-09 23:26:30 +0545

    router mistake fixed and db connection added

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const dotenv = require('dotenv');
-
+const mongoose = require("mongoose");
+dotenv.config({ path: "./config.env" });
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then((con) => console.log("mongoose connected"));
 
 const port = (process.env.PORT = process.env.port || 3000)
 app.listen(port, () => {
