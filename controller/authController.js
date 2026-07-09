165190e5da34655f5bc9de70e89b2b162b121711
Anishh <pokharelanish099@gmail.com>
2026-07-09 23:24:52 +0545

    typo and auth next() fixed

--- controller/authController.js
+++ controller/authController.js
@@ -1,13 +1,24 @@
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
-const User = require("../models/userModel");
+const User = require('./../models/userModel');
 const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError");
-const token = require("morgan");
+const morgan = require("morgan");
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN
+  });
+};
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+
+  res.status(statusCode).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
   });
 };
 
@@ -18,7 +29,7 @@ exports.logIn = catchAsync(async (req, res, next) => {
     next(new AppError("Incorrect emal and password", 400));
   }
 
-  const user = await user.findOne({ email }).select("+password");
+  const user = await User.findOne({ email }).select("+password");
 
   if (!user || !(await user.checkCorrectPassword(password, user.password))) {
     return next(new AppError("Incorrect email id and password", 401));
@@ -34,11 +45,12 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
-    token.req.headers.authorization.split(" ")[1];
+    token=req.headers.authorization.split(" ")[1];
   }
   if (!token || token === "null") {
     return next(
       new AppError("You are not logged in.Please login and try again", 401)
     );
-  }
+    }
+next();
 });
