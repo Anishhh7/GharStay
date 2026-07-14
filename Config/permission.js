6218aeb90f258f49e4e38d9db54932dc6f2ef98c
Anishh <pokharelanish099@gmail.com>
2026-07-14 18:25:59 +0545

    chore: add tasks and roles

--- Config/permission.js
+++ Config/permission.js
@@ -1,58 +1,64 @@
 export default {
- users: {
-  create: ["superAdmin"],
-  readAll: ["superAdmin"],
-  update: ["superAdmin"],
-  delete: ["superAdmin"]
- },
+  users: {
+    create: ['superAdmin'],
+    readAll: ['superAdmin'],
+    update: ['superAdmin'],
+    delete: ['superAdmin'],
+  },
 
- rooms: {
-  create: ["admin", "superAdmin"],
-  update: ["admin", "superAdmin"],
-  delete: ["admin", "superAdmin"]
- },
+  rooms: {
+    create: ['admin', 'superAdmin'],
+    update: ['admin', 'superAdmin'],
+    delete: ['admin', 'superAdmin'],
+  },
 
- packages: {
-  create: ["admin", "superAdmin"],
-  update: ["admin", "superAdmin"],
-  delete: ["admin", "superAdmin"]
- },
+  packages: {
+    create: ['admin', 'superAdmin'],
+    update: ['admin', 'superAdmin'],
+    delete: ['admin', 'superAdmin'],
+  },
 
- menu: {
-  create: ["admin", "superAdmin"],
-  update: ["admin", "superAdmin"],
-  delete: ["admin", "superAdmin"]
- },
- gallery: {
-  create: ["superAdmin"],
-  update: ["superAdmin"],
-  delete: ["superAdmin"]
- },
- events: {
-  create: ["superAdmin"],
-  update: ["superAdmin"],
-  delete: ["superAdmin"]
- },
- blog: {
-  create: ["superAdmin"],
-  update: ["superAdmin"],
-  delete: ["superAdmin"]
- },
- contact: {
-  create: ["superAdmin"],
-  readAll: ["superAdmin"],
-  update: ["superAdmin"],
-  delete: ["superAdmin"]
- },
- faq: {
-  create: ["superAdmin"],
-  update: ["superAdmin"],
-  delete: ["superAdmin"]
- },
- testimonial: {
-  create: ["superAdmin"],
-  readAll: ["superAdmin"],
-  update: ["superAdmin"],
-  delete: ["superAdmin"]
- }
+  menu: {
+    create: ['admin', 'superAdmin'],
+    update: ['admin', 'superAdmin'],
+    delete: ['admin', 'superAdmin'],
+  },
+  gallery: {
+    create: ['superAdmin'],
+    update: ['superAdmin'],
+    delete: ['superAdmin'],
+  },
+  events: {
+    create: ['superAdmin'],
+    update: ['superAdmin'],
+    delete: ['superAdmin'],
+  },
+  blog: {
+    create: ['superAdmin'],
+    update: ['superAdmin'],
+    delete: ['superAdmin'],
+  },
+  contact: {
+    create: ['superAdmin'],
+    readAll: ['superAdmin'],
+    update: ['superAdmin'],
+    delete: ['superAdmin'],
+  },
+  faq: {
+    create: ['superAdmin'],
+    update: ['superAdmin'],
+    delete: ['superAdmin'],
+  },
+  testimonial: {
+    create: ['superAdmin'],
+    readAll: ['superAdmin'],
+    update: ['superAdmin'],
+    delete: ['superAdmin'],
+  },
+  reservation: {
+    readAll: ['superAdmin','admin'],
+    update: ['superAdmin', 'admin'],
+    updateAll: ['superAdmin'],
+    delete: ['superAdmin'],
+  },
 };
