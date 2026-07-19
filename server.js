a5fb3db366c8f55bb6111b0a8b4760adae008193
Anishh <pokharelanish099@gmail.com>
2026-07-19 17:50:31 +0545

    feat: add global error handling for server

--- server.js
+++ server.js
@@ -1,25 +1,41 @@
-import dotenv from 'dotenv'
-dotenv.config({ path: "./Config/config.env" });
-dotenv.config({ path: './Config/cloudinary.env' });
+import dotenv from "dotenv";
 
-import mongoose from 'mongoose';
-import { globalTimestampFormatter } from './utils/mongoosePlugins.js';
+// Catch synchronous errors
+process.on("uncaughtException", (err) => {
+  console.error("UNCAUGHT EXCEPTION!");
+  console.error(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: "./Config/config.env" });
+dotenv.config({ path: "./Config/cloudinary.env" });
+
+import mongoose from "mongoose";
+import { globalTimestampFormatter } from "./utils/mongoosePlugins.js";
 
 mongoose.plugin(globalTimestampFormatter);
 
-const {default:app} = await import('./app.js');
-
-
+const { default: app } = await import("./app.js");
 
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
 );
 
-mongoose.connect(DB).then((con) => console.log("mongoose connected"));
+mongoose.connect(DB).then(() => console.log("MongoDB connected"));
 
+const port = process.env.PORT || 3000;
 
-const port = (process.env.PORT = process.env.port || 3000)
-app.listen(port, () => {
-    console.log(`server connected!! at ${port}`);
-})
+const server = app.listen(port, () => {
+  console.log(`Server connected at ${port}!!!`);
+});
+
+// Catch unhandled promise rejections
+process.on("unhandledRejection", (err) => {
+  console.error("UNHANDLED REJECTION! 💥");
+  console.error(err.name, err.message);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
